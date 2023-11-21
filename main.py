3e1e634103377d923a8b2d35b79edc60a75abad4
Moazzam Riaz <126562859+moazzamdev@users.noreply.github.com>
2023-11-21 23:04:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ def main():
     st.title("Text Classification App")
 
     # Model selection
-    model_selection = st.selectbox("Select Model", ["Model 1", "Model 2", "Model 3"])
+    model_selection = st.selectbox("Select Model", ["Model 1", "Model 2"])
 
     if model_selection == "Model 1":
-        model1()
-    elif model_selection == "Model 2":
-        model3()
-    elif model_selection == "Model 3":
         model2()
+    elif model_selection == "Model 2":
+        model1()
+    
 
 
 
